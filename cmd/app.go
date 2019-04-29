629be050bc3b4f90ebc28a90e22cd0e43d45ae4a
Akihiko Ito <abc@akihiko.eu>
2019-04-29 22:46:08 +0200

    v0.1.0

--- cmd/app.go
+++ cmd/app.go
@@ -15,6 +15,7 @@ func newCLIApp() *cli.App {
 	app := cli.NewApp()
 	app.Name = "lesen"
 	app.Usage = "Command line RSS Reader"
+	app.Version = "0.1.0"
 	app.Commands = []cli.Command{listCommand, tuiCommand}
 	return app
 }
